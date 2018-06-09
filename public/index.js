d05e7828f94c1c0c63bd66f0a0fd46682f882179
Jen Gilbert <jgilbert@lyft.com>
2018-06-09 13:38:51 -0700

    Add subscription capability

--- public/index.js
+++ public/index.js
@@ -1,12 +1,45 @@
+// LIBRARY CODE
+
 function createStore() {
   let state = "I'm the state!";
 
   const getState = () => state;
 
+  const handleStateChange = () => {
+    listeners.map(listener => listener());
+  };
+
+  const testStateChangeForNow = () => {
+    handleStateChange();
+  };
+
+  const listeners = [];
+
+  const subscribe = listener => {
+    console.log("Library: Adding listener.");
+    listeners.push(listener);
+  };
+
   return {
-    getState
+    getState,
+    subscribe,
+    testStateChangeForNow
   };
 }
 
+// APPLICATION CODE
+
 const store = createStore();
+
+console.log("App: The initial state is");
 console.log(store.getState());
+
+const listener = () => {
+  console.log("I'm a listener and I'm running right now!");
+};
+
+console.log("App: Attempting subscription.");
+const unsubscribe = store.subscribe(listener);
+
+console.log("Testing state change.");
+store.testStateChangeForNow();
